3fa0ebdbc929d097ce38255ffef7590977f3b179
Debbiie-creator <63780956+Debbiie-creator@users.noreply.github.com>
2021-03-11 19:32:54 +0530

    Update and rename palindrom_prod.py to Problem4.py

--- Problem4.py
+++ Problem4.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Nov 29 21:00:19 2020
-
-@author: mahaswetamitradeb
-"""
+#Brute Force
 n = 0
 for a in range(999, 100, -1):
     for b in range(a, 100, -1):
